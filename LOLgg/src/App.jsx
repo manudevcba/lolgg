32460e1fdcfe72fba8ccbbf9db364cf1e0c82242
manu <manulol400@gmail.com>
2024-09-18 19:34:01 -0300

    udapte changes
    asda

--- LOLgg/src/App.jsx
+++ LOLgg/src/App.jsx
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { useFetch } from './useFetch'
 
-const API_KEY = 'RGAPI-9f80c33e-8c6d-4f2c-9449-98305b33f19c'
+const API_KEY = 'RGAPI-dc2eefb1-e8da-47aa-a8f8-f0161d16f0f9'
 
 function App () {
-  const [buscarPlayer, setBuscarPlayer] = useState('')
+  const [buscarNick, setBuscarNick] = useState('')
+  const [buscarTag, setBuscarTag] = useState('')
+  const [url, setUrl] = useState('')
 
-  const buscarUnPlayer = (e) => {
-    const API_NUCLEO = 'la2.api.riotgames.com'
+  console.log(buscarNick)
+  console.log(buscarTag)
+
+  const { data, loading, error } = useFetch(url)
+
+  const buscarPlayer = (event) => {
+    event.preventDefault()
+    if (buscarNick && buscarTag) {
+      const API_NUCLEO = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${buscarNick}/${buscarTag}?api_key=${API_KEY}`
+      setUrl(API_NUCLEO)
+    }
   }
 
   return (
     <>
 
       <h1>LOL.GG</h1>
-      <h2>Ingrese su nick+id de lol uwu</h2>
-      <input type='text' placeholder='marinero#7218' onChange={e => setBuscarPlayer(e.target.value)} />
-      <button onClick={e => buscarUnPlayer(e)}>buscar perfil</button>
+      <h2>Ingrese su nick arriba y tag abajo de lol </h2>
+      <input type='text' placeholder='nick = marinero' onChange={e => setBuscarNick(e.target.value)} />
+      <input type='text' placeholder='tag = 7218' onChange={e => setBuscarTag(e.target.value)} />
+      <button onClick={e => buscarPlayer(e)}>buscar perfil</button>
 
     </>
   )
