456f81b6cc2d157064e08a97b2f8c9aa545bec1d
manu <manulol400@gmail.com>
2024-09-20 21:28:47 -0300

    ver cosas del error de la request api
    lpm errores en la request CORPS

--- LOLgg/src/App.jsx
+++ LOLgg/src/App.jsx
@@ -1,52 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
-import { useFetch } from './useFetch'
-
-const API_KEY = 'RGAPI-dc2eefb1-e8da-47aa-a8f8-f0161d16f0f9'
 
 function App () {
+  const API_KEY = 'RGAPI-172c0ced-da1d-45fc-bd67-dbe1657a3c3d'
   const [buscarNick, setBuscarNick] = useState('')
   const [buscarTag, setBuscarTag] = useState('')
-  const [url1, setUrl1] = useState('')
-  const [url2, setUrl2] = useState('')
+  const [playerData, setPlayerData] = useState({})
 
-  console.log(buscarNick)
-  console.log(buscarTag)
+  const nick = encodeURIComponent(buscarNick)
+  const tag = encodeURIComponent(buscarTag)
 
-  const handleSearch = () => {
-    if (buscarNick && buscarTag) {
-      const API_NUCLEO = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${buscarNick}/${buscarTag}?api_key=${API_KEY}`
-      setUrl1(API_NUCLEO)
-    }
+  function buscarPlayer (e) {
+    const API_NUCLEO = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${nick}/${tag}?api_key=${API_KEY}`
+
+    fetch(API_NUCLEO)
+      .then(response => response.json())
+      .then(response => {
+        const data = response
+        setPlayerData(data)
+        console.log(playerData)
+      })
+    console.log(playerData)
   }
-
-  const { data, loading, error } = useFetch(url1)
-  console.log(url1)
-
-  useEffect(() => {
-    if (data && data.puuid) {
-      const puuId = data.puuid // Solo accede a puuId si data está disponible
-      const API_LOL = `https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuId}?api_key=${API_KEY}`
-      if (url2 !== API_LOL) {
-        setUrl2(API_LOL)
-      }
-    }
-  }, [data])
 
   return (
     <>
-      <h1>LOL.GG</h1>
-      <h2>Ingrese su nick arriba y tag abajo de lol</h2>
+
+      <img href='https://ibb.co/M8z5svw' src='https://i.ibb.co/M8z5svw/lgo.png' alt='lgo' border='0' />
+      <h2 className='texth2'>Ingrese su nick y tag de lol</h2>
       <input type='text' placeholder='nick = marinero' onChange={e => setBuscarNick(e.target.value)} />
       <input type='text' placeholder='tag = 7218' onChange={e => setBuscarTag(e.target.value)} />
+      <button onClick={e => buscarPlayer(e)}>buscar sumoner</button>
+      <br /> <br />
 
-      {/* Botón para realizar la búsqueda */}
-      <button onClick={handleSearch}>Buscar Perfil</button>
-
-      <div>
-        {error && <div>Error: {error}</div>}
-        {loading && <div>Loading...</div>}
-      </div>
     </>
   )
 }
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # lolgg
 Proyecto LOLgg
+
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const API_KEY = 'RGAPI-172c0ced-da1d-45fc-bd67-dbe1657a3c3d'
+
+function App () {
+  const [buscarNick, setBuscarNick] = useState('')
+  const [buscarTag, setBuscarTag] = useState('')
+  const [url1, setUrl1] = useState('')
+  const [datosAcc, setDatosAcc] = useState(null)
+
+  const buscarPuuid = () => {
+    if (buscarNick && buscarTag) {
+      const nick = encodeURIComponent(buscarNick)
+      const tag = encodeURIComponent(buscarTag)
+      const API_NUCLEO = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${nick}/${tag}?api_key=${API_KEY}`
+      return setUrl1(API_NUCLEO)
+    }
+  }
+  buscarPuuid()
+  console.log(url1)
+
+  function buscarPlayer (e) {
+    buscarPuuid()
+
+    console.log('hola')
+  }
+
+  useEffect(() => {
+    fetch(url1)
+      .then((response) => response.json())
+      .then((data) => {
+        const puuid = data.puuid
+
+        return fetch(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`)
+      })
+      .then((accResponse) => accResponse.json())
+      .then((accData) => setDatosAcc(accData))
+  }, [])
+
+  return (
+    <>
+
+      <img href='https://ibb.co/M8z5svw' src='https://i.ibb.co/M8z5svw/lgo.png' alt='lgo' border='0' />
+      <h2 className='texth2'>Ingrese su nick y tag de lol</h2>
+      <input type='text' placeholder='nick = marinero' onChange={e => setBuscarNick(e.target.value)} />
+      <input type='text' placeholder='tag = 7218' onChange={e => setBuscarTag(e.target.value)} />
+      <button onClick={e => buscarPlayer(e)}>buscar sumoner</button>
+      <br /> <br />
+
+    </>
+  )
+}
+
+export default App
