c2817c7c5f6b57039c6bc3e23ccd7216a0b8f12d
manu <manulol400@gmail.com>
2024-09-23 16:50:29 -0300

    implemente proxy para request api
    implemente proxy para request api

--- LOLgg/src/App.jsx
+++ LOLgg/src/App.jsx
@@ -1,27 +1,22 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App () {
-  const API_KEY = 'RGAPI-5c7a7140-e92a-4a95-be38-1c2d9e5eff9e'
   const [buscarNick, setBuscarNick] = useState('')
   const [buscarTag, setBuscarTag] = useState('')
   const [playerData, setPlayerData] = useState({})
 
-  const nick = encodeURIComponent(buscarNick)
-  const tag = encodeURIComponent(buscarTag)
-
-  function buscarPlayer (e) {
-    const API_NUCLEO = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${nick}/${tag}?api_key=${API_KEY}`
-
-    fetch(API_NUCLEO)
-      .then(response => response.json())
-      .then(response => {
-        const data = response
-        setPlayerData(data)
-        console.log(playerData)
+  function buscarPlayer (event) {
+    axios.get('http://localhost:4000/infoAcc')
+      .then(function (response) {
+        setPlayerData(response.data)
+      }).catch(function (error) {
+        console.log(error)
       })
-    console.log(playerData)
   }
+
+  console.log(playerData)
 
   return (
     <>
@@ -30,7 +25,7 @@ function App () {
       <h2 className='texth2'>Ingrese su nick y tag de lol</h2>
       <input type='text' placeholder='nick = marinero' onChange={e => setBuscarNick(e.target.value)} />
       <input type='text' placeholder='tag = 7218' onChange={e => setBuscarTag(e.target.value)} />
-      <button onClick={e => buscarPlayer(e)}>buscar sumoner</button>
+      <button onClick={buscarPlayer}>buscar sumoner</button>
       <br /> <br />
 
     </>
--- proxyServer.js
+++ proxyServer.js
@@ -6,7 +6,29 @@ const app = express()
 
 app.use(cors())
 
-const API_KEY = 'RGAPI-5c7a7140-e92a-4a95-be38-1c2d9e5eff9e'
+const API_KEY = 'RGAPI-3d83766d-4304-4d64-b881-1a7fd06491c5'
+
+function getPlayerPUUID (playerNick, playerTag) {
+  return axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${playerNick}/${playerTag}?api_key=${API_KEY}`)
+    .then(response => {
+      console.log(response.data)
+      return response.data.puuid
+    }).catch(err => err)
+}
+
+app.get('/infoAcc', async (req, res) => {
+  const playerNick = 'marinero'
+  const playerTag = '7218'
+
+  const PUUID = await getPlayerPUUID(playerNick, playerTag)
+  const API_CALL = `https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${PUUID}?api_key=${API_KEY}`
+
+  const infoAcc = await axios.get(API_CALL)
+    .then(response => response.data)
+    .catch(err => err)
+  console.log(infoAcc)
+  res.json(infoAcc)
+})
 
 app.listen(4000, function () {
   console.log('server started on port 4000')
